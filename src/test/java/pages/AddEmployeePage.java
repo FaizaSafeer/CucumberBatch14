bce6e5aa18827927f47c3fb50ebf1a83174641af
faiza <faiza.safeer456@gmail.com>
2022-12-15 13:30:58 +1100

    class2

--- src/test/java/pages/AddEmployeePage.java
+++ src/test/java/pages/AddEmployeePage.java
@@ -12,6 +12,26 @@ public class AddEmployeePage extends CommonMethods {
     @FindBy(id = "lastName")
     public WebElement lastNameField;
 
+    @FindBy(id ="middleName")
+    public WebElement middleNameField;
+
+    @FindBy(id = "photofile")
+    public WebElement photograph;
+
+    @FindBy(id = "chkLogin")
+    public WebElement checkBox;
+
+    @FindBy(id = "user_name")
+    public WebElement createusernameField;
+
+    @FindBy(id = "user_password")
+    public WebElement createpasswordField;
+
+    @FindBy(id = "re_password")
+    public WebElement confirmpasswordField;
+
+    @FindBy(id = "employeeId")
+    public WebElement empIdLocator;
     @FindBy(id = "btnSave")
     public WebElement saveButton;
 
--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -7,21 +7,19 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         //features we use to provide the path of all the feature files
-        features = "src/test/resources/features/",
+        features = "@target/failed.txt",
         glue = "steps",
         //when you set dry run to true, it stops actual execution
         //it will quickly scan all the gherkin steps whether they are implemented or not
         //when we set dry run to false, it starts execution again
-        dryRun = false,
-        tags = "@tc1101",
+        // dryRun = false,
+        // tags = "@tc1101",
         //to remove irrelavant information from console, you need to set monochrome to true
         monochrome = true,
         //pretty keywords prints the steps in the console to increase readability
-       // to generate the reporte we need plug of runner class
-        plugin = {"pretty","html:target/cucmber.html","json:target/cucumber.json",
-                "rerun:target/failed.txt"}
-
-
+        //to generate the reports we need plugin of runner class
+        plugin = {"pretty"}
 )
-public class SmokeRunner {
+
+public class FailedRunner {
 }
